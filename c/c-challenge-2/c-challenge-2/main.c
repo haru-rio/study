888d92d2d63e103f71c99a81daba9804e126f9ae
Ryo Okuda <haru.rio.ph@gmail.com>
2020-01-28 03:34:55 +0900

    create c-challenge-2

---
+++ c/c-challenge-2/c-challenge-2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c-challenge-2
+//
+//  Created by 奥田亮 on 2020/01/28.
+//  Copyright © 2020 Nisi and Okuda. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
