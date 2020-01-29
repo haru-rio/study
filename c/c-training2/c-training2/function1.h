528a2e0a460b9208bab1e0ffb1672c0e2efd123d
Ryo Okuda <haru.rio.ph@gmail.com>
2020-01-30 00:58:04 +0900

    すべての処理を関数に分割

--- c/c-training2/c-training2/function1.h
+++ c/c-training2/c-training2/function1.h
@@ -10,6 +10,13 @@
 #define function1_h
 
 void input_check(void);
+void score_check(void);
+void age_check(void);
+void doraemon_switchvoid(void);
+void sex_check(void);
+void for_loop_print(void);
+void while_loop(void);
+void circle_radius_do_while(void);
 
 #include <stdio.h>
 
